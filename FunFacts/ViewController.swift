9c4c4eb52c6a8b65d886ab40552c4bb36a3125f4
Smitha Kolan <smitkolan@gmail.com>
2019-05-31 18:23:40 +0200

    Initial Commit

---
+++ FunFacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FunFacts
+//
+//  Created by Smitha Kolan on 5/31/19.
+//  Copyright © 2019 Smitha Kolan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FunFactsTests/FunFactsTests.swift
@@ -0,0 +1,34 @@
+//
+//  FunFactsTests.swift
+//  FunFactsTests
+//
+//  Created by Smitha Kolan on 5/31/19.
+//  Copyright © 2019 Smitha Kolan. All rights reserved.
+//
+
+import XCTest
+@testable import FunFacts
+
+class FunFactsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
